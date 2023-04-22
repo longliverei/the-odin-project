c03eeeaac285e5087a735da0dd0d33eeca21f3ee
Reinaldo Yabiku <reinaldoybk@hotmail.com>
2023-04-21 21:35:28 -0300

    add play round function

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,5 +1,5 @@
 const getComputerChoice = () => {
-    let  choicesArray = [
+    const  choicesArray = [
         "rock",
         "paper",
         "scissor"
@@ -16,3 +16,23 @@ const getComputerChoice = () => {
             return "scissor";
     }
 }
+
+const playRound = (playerSelection, computerSelection) => {
+    const playerChoice = playerSelection.toLowerCase();
+
+    if (playerChoice == computerSelection) {
+        return "It's a draw!";
+    }
+
+    if (playerChoice == "rock") {
+        return computerSelection == "scissor" ? "You win! Rock beats Scissor" : "You lose! Paper beats Rock";
+    } else if (playerChoice == "paper") {
+        return computerSelection == "rock" ? "You win! Paper beats Rock" : "You lose! Scissor beats Paper";
+    } else {
+        return computerSelection == "paper" ? "You win! Scissor beats paper" : "You lose! Rock beats Scissor";
+    } 
+}
+
+const playerSelection = "Rock";
+
+console.log(playRound(playerSelection, getComputerChoice()));
