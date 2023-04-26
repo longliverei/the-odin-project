8025654f18d9ee493502f37adb3fdf905e01cce8
Reinaldo Yabiku <reinaldoybk@hotmail.com>
2023-04-25 21:57:27 -0300

    feat for score and results

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -17,53 +17,46 @@ const getComputerChoice = () => {
     }
 }
 
+
+
 const playRound = (playerSelection, computerSelection) => {
     const playerChoice = playerSelection.toLowerCase();
-
+    
+    
     if (playerChoice == computerSelection)
     return "draw";
-
+        
     if (playerChoice == "rock") {
         return computerSelection == "scissor" ? "win" : "lose";
     } else if (playerChoice == "paper") {
         return computerSelection == "rock" ? "win" : "lose";
     } else {
         return computerSelection == "paper" ? "win" : "lose";
-    } 
+    }
 };
 
+
+let playerScore = 0;
+let computerScore = 0;
 const buttons = document.querySelectorAll("button");
+const score = document.querySelector(".score");
+score.textContent = `${playerScore} x ${computerScore}`
+const result = document.querySelector(".result");
+
 
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
         const choice = button.id;
-        console.log(playRound(choice, getComputerChoice()));
-    });
-});
-
-//const game = () => {
-//    let playerScore = 0, computerScore = 0;
-//    let winner = "Draw game";
-//
-//    for (let i = 0; i < 5; i++) {
-//        let playerChoice = prompt("Jan, ken, po!");
-//        let computerChoice = getComputerChoice();
-//        const res = playRound(playerChoice, computerChoice);
-//        if (res == "win") {
-//            console.log("You win this round! " + playerChoice + " beats " + computerChoice + " Score: " + ++playerScore + " x " + computerScore);
-//        } else if (res == "lose") {
-//            console.log("You lose this round! " + computerChoice + " beats " + playerChoice + " Score: " + playerScore + " x " + ++computerScore);
-//        } else {
-//            console.log("It's a draw :) your choice: " + playerChoice + " Computer choice: " + computerChoice);
-//        }
-//    }
-//
-//    if (playerScore == computerScore) {
-//        return winner;
-//    }
-//
-//    playerScore > computerScore ? winner = "Player 1" : winner = "Computer";
-//    
-//    return winner + " wins";
-//}
-
+        const pcChoice = getComputerChoice();
+        const res = playRound(choice, pcChoice);
+        if (res == "win") {
+            result.textContent = `You win! ${choice} beats ${pcChoice}`;
+            score.textContent = `${++playerScore} x ${computerScore}`;
+        } else if (res == "lose") {
+            result.textContent = `You lose! ${pcChoice} beats ${choice}`;
+            score.textContent = `${playerScore} x ${++computerScore}`;
+        } else {
+            result.textContent = `It's a draw!`;
+        }
+    })
+})
